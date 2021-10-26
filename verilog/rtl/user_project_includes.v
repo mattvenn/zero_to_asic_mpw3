8ed13acd52de9f10f15b16ca5abc3923881bbca1
matt venn <matt@mattvenn.net>
2021-10-26 18:38:21 +0200

    updated with ws2812 and mpw-3a hardened macros

--- verilog/rtl/user_project_includes.v
+++ verilog/rtl/user_project_includes.v
@@ -5,10 +5,12 @@
 // | 1          | Frequency counter | Matt Venn                    | https://github.com/mattvenn/wrapped_frequency_counter | mpw3   |
 // | 2          | VGA Clock         | Matt Venn                    | https://github.com/mattvenn/wrapped_vga_clock         | main   |
 // | 3          | TPM2137           | Q3K & Matt Venn              | https://github.com/mattvenn/wrapped_tpm2137           | main   |
+// | 4          | WS2812            | Matt Venn                    | https://github.com/mattvenn/wrapped_ws2812            | main   |
 // | 5          | Zube              | Jonathan 'theJPster' Pallant | https://github.com/thejpster/zube_submission          | v1.2.0 |
 // +------------+-------------------+------------------------------+-------------------------------------------------------+--------+
 `include "wrapped_rgb_mixer/wrapper.v" // 0
 `include "wrapped_frequency_counter/wrapper.v" // 1
 `include "wrapped_vga_clock/wrapper.v" // 2
 `include "wrapped_tpm2137/wrapper.v" // 3
+`include "wrapped_ws2812/wrapper.v" // 4
 `include "zube_submission/wrapper.v" // 5
